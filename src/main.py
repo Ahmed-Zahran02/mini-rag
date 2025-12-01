772362d7ed6f71358fdfbfab8c38951998ac95d9
Ahmed Zahran <medozahran02@gmail.com>
2025-12-01 08:13:57 +0200

    added some fixes to database connection

--- src/main.py
+++ src/main.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers import get_settings
 import uvicorn
 
-app = FastAPI()
 
-
-@app.on_event("startup")
-async def startup():
+@app.asynccontextmanager
+async def lifespan(app: FastAPI):
     settings = get_settings()
     app.state.connection = AsyncIOMotorClient(settings.DATABASE_URL)
     app.state.db_client = app.state.connection[settings.DATABASE_NAME]
+    yield
+    app.state.connection.close()
 
 
+app = FastAPI(lifespan=lifespan)
 app.include_router(base.base_router)
 app.include_router(data.data_router)
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    if hasattr(app.state, "client"):
-        await app.state.client.close()
 
 
 # app = FastAPI()

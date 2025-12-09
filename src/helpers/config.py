3be81bd8b2a328ef4d2e6b979cc84efbe629a33e
Ahmed Zahran <medozahran02@gmail.com>
2025-12-09 11:02:46 +0200

    Update congigurations

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -13,6 +13,19 @@ class Settings(BaseSettings):
     CHUNK_SIZE: int  # 500 KB
     DATABASE_URL: str
     DATABASE_NAME: str
+
+    GENERATION_BACKEND="OPENAI"
+    EMBEDDING_BACKEND="COHERE"
+    OPENAI_API_URL: str 
+    COHERE_API_KEY: str
+
+    GENERATION_MODEL_ID: str
+    EMBEDDING_MODEL_ID: str
+    EMBEDDING_MODEL_SIZE:int
+
+    INPUT_MAX_CHARACTERS:int
+    GENERATION_MAX_TOKENS:int
+    GENERATION_TEMPERATURE:int
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 

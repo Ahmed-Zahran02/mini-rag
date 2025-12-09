616ca0c5cd86e0c4a4ad4268c07537faa667d214
Ahmed Zahran <medozahran02@gmail.com>
2025-12-09 11:05:21 +0200

    added the llm logic to main

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers import get_settings
 import uvicorn
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 
 @asynccontextmanager
@@ -11,6 +12,13 @@ async def lifespan(app: FastAPI):
     settings = get_settings()
     app.state.connection = AsyncIOMotorClient(settings.DATABASE_URL)
     app.state.db_client = app.state.connection[settings.DATABASE_NAME]
+    llm_factory = LLMProviderFactory(config=settings)
+    app.state.generation_client = llm_factory.get_provider(settings.GENERATION_BACKEND)
+    app.state.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+    app.state.embedding_client = llm_factory.get_provider(settings.EMBEDDING_BACKEND)
+    app.state.embedding_client.set_embedding_model(
+        settings.EMBEDDING_MODEL_ID, settings.EMBEDDING_MODEL_SIZE
+    )
     yield
     app.state.connection.close()
   

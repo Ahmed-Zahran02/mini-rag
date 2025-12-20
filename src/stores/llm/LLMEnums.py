64b91929b18af26884346b149000e24cab9f8444
Ahmed Zahran <medozahran02@gmail.com>
2025-12-20 07:06:43 +0200

    Add gemini in enums. Add the required response enums. fix typo in cohere enums

--- src/stores/llm/LLMEnums.py
+++ src/stores/llm/LLMEnums.py
@@ -4,6 +4,7 @@ from enum import Enum
 class LLMProviderEnums(Enum):
     OPENAI = "openai"
     COHERE = "cohere"
+    GEMINI = "gemini"
 
 
 class OpenAIRoles(Enum):
@@ -19,8 +20,21 @@ class CoHereRoles(Enum):
     TOOL = "tool"
 
 
-class CoHereInputTypes(Enum):
+class DocumentTypes(Enum):
     DOCUMENT = "search_document"
     QUERY = "search_query"
     CLASSIFICATION = "classification"
     CLUSTERING = "clustering"
+
+
+class GeminiRoles(Enum):
+    USER = "user"
+    ASSISTANT = "model"
+
+
+class GeminiInputTypes(Enum):
+    RETRIEVAL_QUERY = "RETRIEVAL_QUERY"
+    RETRIEVAL_DOCUMENT = "RETRIEVAL_DOCUMENT"
+    SEMANTIC_SIMILARITY = "SEMANTIC_SIMILARITY"
+    CLASSIFICATION = "CLASSIFICATION"
+    CLUSTERING = "CLUSTERING"

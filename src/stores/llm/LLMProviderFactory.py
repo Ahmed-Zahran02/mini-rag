6075ac8a40ee2fe642b25076b10db8cbf8e622b3
Ahmed Zahran <medozahran02@gmail.com>
2025-12-21 08:36:38 +0200

    Fix parameter mismatch

--- src/stores/llm/LLMProviderFactory.py
+++ src/stores/llm/LLMProviderFactory.py
@@ -11,22 +11,22 @@ class LLMProviderFactory:
             return OpenAIProvider(
                 api_key=self.config.OPENAI_API_KEY,
                 base_url=self.config.OPENAI_BASE_URL,
-                max_input_characters=self.config.MAX_INPUT_CHARACTERS,
-                max_tokens=self.config.MAX_TOKENS,
+                max_input_characters=self.config.INPUT_MAX_CHARACTERS,
+                max_tokens=self.config.GENERATION_MAX_TOKENS,
                 temperature=self.config.TEMPERATURE,
             )
         elif provider_name == LLMProviderEnums.COHERE.value:
             return CoHereProvider(
                 api_key=self.config.COHERE_API_KEY,
-                max_input_characters=self.config.MAX_INPUT_CHARACTERS,
-                max_tokens=self.config.MAX_TOKENS,
+                max_input_characters=self.config.INPUT_MAX_CHARACTERS,
+                max_tokens=self.config.GENERATION_MAX_TOKENS,
                 temperature=self.config.TEMPERATURE,
             )
         elif provider_name == LLMProviderEnums.GEMINI.value:
             return GeminiProvider(
                 api_key=self.config.GEMINI_API_KEY,
-                max_input_characters=self.config.MAX_INPUT_CHARACTERS,
-                max_tokens=self.config.MAX_TOKENS,
+                max_input_characters=self.config.INPUT_MAX_CHARACTERS,
+                max_tokens=self.config.GENERATION_MAX_TOKENS,
                 temperature=self.config.TEMPERATURE,
             )
         return None
